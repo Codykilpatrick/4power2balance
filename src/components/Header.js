8cd62ec2c71cd8b3a11089a9dd26c769527f8a6e
aquasaphir <aqua995.ste@gmail.com>
2024-10-28 22:53:22 +0100

    path corrected

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="bg-gradient-to-r from-nebula-purple to-space-dark text-starlight-white p-6 text-center shadow-md shadow-black">
       <h1 className="text-4xl font-extrabold tracking-wide">
-        <img src="public/4Power2BalanceWhite.png" alt="4 Power 2 Balance"/>
+        <img src="/public/4Power2BalanceWhite.png" alt="4 Power 2 Balance"/>
       </h1>
       <div className="mt-2 border-t-2 border-starlight-white w-1/4 mx-auto"></div>
     </header>

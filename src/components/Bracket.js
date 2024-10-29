a507316593fb3edd82887d7746a58965c8062ee6
Cody Kilpatrick <codykilpatrick1996@gmail.com>
2024-10-29 07:54:57 -0400

    WIP

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -1,73 +1,35 @@
 import React from 'react';
 
 const SwissBracket = ({ results }) => {
-    const teamlist = results.map((result) => result.name);
     const teamStandingList = results.map((result) => [result.name, result.points, result.matchesPlayed]);
-    const matchList = results.map((result) => [result.name, result.currentPair]);
-
+    const matchList = results.map((result) => [result.name, result.currentPair, result.matchTime]);
 
   return (
     <div className="space-y-8">
-      {/* <div className="space-y-6">
+      <div className="space-y-6">
         <h2 className="text-3xl font-bold text-ionized-blue border-b-2 border-ionized-blue pb-2 out">Current Week's Matches</h2>
+          {matchList
+            .map((match, index) => (           
             <div
+              index={index}
               className="bg-space-dark p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 outline outline-starlight-white"
             >
               <div className="p-2 text-starlight-white text-xl font-semibold border-b border-metallic-grey">
-                <span>Panda</span>
+                <span>{match[0]}</span>
               </div>
                 <div className="p-2 text-starlight-white text-xl font-semibold">
-                  <span>Loong</span>
+                  <span>{match[0]}</span>
                 </div>
             </div>
-            <div
-              className="bg-space-dark p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 outline outline-starlight-white"
-            
-            >
-                <div className="p-2 text-starlight-white text-xl font-semibold border-b border-metallic-grey">
-                <span>Frontier Negotiator</span>
-              </div>
-                <div className="p-2 text-starlight-white text-xl font-semibold">
-                  <span>Suneaters</span>
-                </div>
-            </div>
-            <div
-              className="bg-space-dark p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 outline outline-starlight-white"
-            >
-                <div className="p-2 text-starlight-white text-xl font-semibold border-b border-metallic-grey">
-                <span>I'd Sin for that Solar Empire</span>
-              </div>
-                <div className="p-2 text-starlight-white text-xl font-semibold">
-                  <span>Bankstreet Boys</span>
-                </div>
-            </div>
-            <div
-              className="bg-space-dark p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 outline outline-starlight-white"
-            >
-                <div className="p-2 text-starlight-white text-xl font-semibold border-b border-metallic-grey">
-                <span>Two Eggs, One Kol, and a Pure Eradica</span>
-              </div>
-                <div className="p-2 text-starlight-white text-xl font-semibold">
-                  <span>LirTnolikSimcler2xdd</span>
-                </div>
-            </div>
-            <div
-              className="bg-space-dark p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 outline outline-starlight-white"
-            >
-                <div className="p-2 text-starlight-white text-xl font-semibold border-b border-metallic-grey">
-                <span>ISS</span>
-              </div>
-                <div className="p-2 text-starlight-white text-xl font-semibold">
-                  <span>The Nameless Ones</span>
-                </div>
-            </div>
-      </div> */}
+            )) 
+          }
+      </div>
       <div className="mt-12">
         <h2 className="text-3xl font-bold text-ionized-blue border-b-2 border-ionized-blue pb-2">Standings</h2>
         <ul className="space-y-4">
           {teamStandingList
             .map((team, index) => (
-              <li key={team.name} className="flex flex-col space-y-2 bg-space-dark p-4 rounded-lg shadow-md">
+              <li key={team[0]} className="flex flex-col space-y-2 bg-space-dark p-4 rounded-lg shadow-md">
                 <div className="flex justify-between items-center text-starlight-white text-lg font-medium">
                   <span>{index + 1}. {team[0]}</span>
                   <span className="bg-ionized-blue text-space-dark px-3 py-1 rounded-full text-sm font-bold">
--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,8 +1,8 @@
 // src/resultsData.js
 const teamData = [
+  { name: 'Bankstreet Boys', points: 0, matchesPlayed: 0, currentPair: 1, matchTime: null },
+  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 0, currentPair: 1, matchTime: null },
   { name: 'Fronteir Negotiator', points: 0, matchesPlayed: 0, currentPair: null, matchTime: null },
-  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 0, currentPair: null, matchTime: null },
-  { name: 'Bankstreet Boys', points: 0, matchesPlayed: 0, currentPair: null, matchTime: null },
   { name: 'Two Eggs, One Kol, and a Pure Eradica', points: 0, matchesPlayed: 0 },
   { name: 'ISS', points: 0, matchesPlayed: 0, currentPair: null, matchTime: null },
   { name: 'LirTnolikSimcler2xdd', points: 0, matchesPlayed: 0, currentPair: null, matchTime: null },

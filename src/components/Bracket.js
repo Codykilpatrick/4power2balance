3d16d48ddef51f31e8ffb440ca52ef84c4a8cf4c
Cody Kilpatrick <codykilpatrick1996@gmail.com>
2024-10-29 07:38:22 -0400

    Update standigns to use new data structure

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -61,27 +61,26 @@ const SwissBracket = ({ results }) => {
                   <span>The Nameless Ones</span>
                 </div>
             </div>
-      </div>
+      </div> */}
       <div className="mt-12">
         <h2 className="text-3xl font-bold text-ionized-blue border-b-2 border-ionized-blue pb-2">Standings</h2>
         <ul className="space-y-4">
-          {standings
-            .sort((a, b) => b.points - a.points || a.name.localeCompare(b.name))
+          {teamStandingList
             .map((team, index) => (
               <li key={team.name} className="flex flex-col space-y-2 bg-space-dark p-4 rounded-lg shadow-md">
                 <div className="flex justify-between items-center text-starlight-white text-lg font-medium">
-                  <span>{index + 1}. {team.name}</span>
+                  <span>{index + 1}. {team[0]}</span>
                   <span className="bg-ionized-blue text-space-dark px-3 py-1 rounded-full text-sm font-bold">
-                    {team.points} points
+                    {team[1]} points
                   </span>
                 </div>
                 <div className="text-sm text-metallic-grey">
-                  Matches Played: {team.matchesPlayed}
+                  Matches Played: {team[2]}
                 </div>
               </li>
             ))}
         </ul>
-      </div> */}
+      </div>
     </div>
   );
   

85c1e791f49cc2202b2f9841b0721be384392207
Cody Kilpatrick <codykilpatrick1996@gmail.com>
2024-10-29 07:24:16 -0400

    Update data structure

--- src/components/resultsData.js
+++ src/components/resultsData.js
@@ -1,15 +1,15 @@
 // src/resultsData.js
 const resultsData = [
-  { name: 'Fronteir Negotiator', points: 0, matchesPlayed: 0 },
-  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 0 },
-  { name: 'Bankstreet Boys', points: 0, matchesPlayed: 0 },
+  { name: 'Fronteir Negotiator', points: 0, matchesPlayed: 0, currentPair: null, matchTime: null },
+  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 0, currentPair: null, matchTime: null },
+  { name: 'Bankstreet Boys', points: 0, matchesPlayed: 0, currentPair: null, matchTime: null },
   { name: 'Two Eggs, One Kol, and a Pure Eradica', points: 0, matchesPlayed: 0 },
-  { name: 'ISS', points: 0, matchesPlayed: 0 },
-  { name: 'LirTnolikSimcler2xdd', points: 0, matchesPlayed: 0 },
-  { name: 'The Nameless Ones', points: 0, matchesPlayed: 0 },
-  { name: 'Suneaters', points: 0, matchesPlayed: 0 },
-  { name: 'Panda', points: 0, matchesPlayed: 0 },
-  { name: 'Loong', points: 0, matchesPlayed: 0 },
+  { name: 'ISS', points: 0, matchesPlayed: 0, currentPair: null, matchTime: null },
+  { name: 'LirTnolikSimcler2xdd', points: 0, matchesPlayed: 0, currentPair: null, matchTime: null },
+  { name: 'The Nameless Ones', points: 0, matchesPlayed: 0, currentPair: null, matchTime: null },
+  { name: 'Suneaters', points: 0, matchesPlayed: 0, currentPair: null, matchTime: null },
+  { name: 'Panda', points: 0, matchesPlayed: 0, currentPair: null, matchTime: null },
+  { name: 'Loong', points: 0, matchesPlayed: 0, currentPair: null, matchTime: null },
 ];
 
 export default resultsData;

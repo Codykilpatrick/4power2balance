251cd7c13aa730421a4471cb1c39e5b8572e6eda
aquasaphir <aqua995.ste@gmail.com>
2024-11-03 17:41:29 +0100

    Status logic eamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -2,14 +2,14 @@
 const teamData = [
   { name: 'Bankstreet Boys', points: 0, matchesPlayed: 0, currentPair: 1, matchTime: '05 NOV 1900 CET' },
   { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 0, currentPair: 1, matchTime: '05 NOV 1900 CET' },
-  { name: 'Frontier Negotiator', points: 0, matchesPlayed: 1, currentPair: 3, matchTime: '02 NOV 2230 CET' },
-  { name: 'Two Eggs, One Kol, and a Pure Eradica', points: 0, matchesPlayed: 1,  currentPair: 4, matchTime: '03 NOV 1500 CET' },
-  { name: 'ISS', points: 0, matchesPlayed: 1, currentPair: 5, matchTime: '03 NOV 1500 CET' },
-  { name: 'LirTnolikSimcler2xdd', points: 3, matchesPlayed: 1, currentPair: 4, matchTime: '03 NOV 1500 CET' },
-  { name: 'The Nameless Ones', points: 3, matchesPlayed: 1, currentPair: 5, matchTime: '03 NOV 1500 CET' },
-  { name: 'Suneaters', points: 3, matchesPlayed: 1, currentPair: 3, matchTime: '02 NOV 2230 CET' },
-  { name: 'Panda', points: 3, matchesPlayed: 1, currentPair: 2, matchTime: 'waiting for replay' },
-  { name: 'Loong', points: 0, matchesPlayed: 1, currentPair: 2, matchTime: 'waiting for replay' },
+  { name: 'Frontier Negotiator', points: 0, matchesPlayed: 1, currentPair: 3, matchTime: 'Suneaters won' },
+  { name: 'Two Eggs, One Kol, and a Pure Eradica', points: 0, matchesPlayed: 1,  currentPair: 4, matchTime: 'LirTnolikSimcler2xdd won' },
+  { name: 'ISS', points: 0, matchesPlayed: 1, currentPair: 5, matchTime: 'The Nameless Ones won' },
+  { name: 'LirTnolikSimcler2xdd', points: 3, matchesPlayed: 1, currentPair: 4, matchTime: 'LirTnolikSimcler2xdd won' },
+  { name: 'The Nameless Ones', points: 3, matchesPlayed: 1, currentPair: 5, matchTime: 'The Nameless Ones won' },
+  { name: 'Suneaters', points: 3, matchesPlayed: 1, currentPair: 3, matchTime: 'Suneaters won' },
+  { name: 'Panda', points: 3, matchesPlayed: 1, currentPair: 2, matchTime: 'Panda won' },
+  { name: 'Loong', points: 0, matchesPlayed: 1, currentPair: 2, matchTime: 'Panda won' },
 ];
 
 export default teamData;

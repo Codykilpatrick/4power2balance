e813e9d67a676b3fcf6ffd2fd1bc8273645851d5
aquasaphir <aqua995.ste@gmail.com>
2024-11-11 22:24:56 +0100

    first Draft done teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -2,11 +2,11 @@
 const teamData = [
   { name: 'Bankstreet Boys', points: 3, matchesPlayed: 1, currentPair: 2, matchTime: 'TBA' },
   { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 1, currentPair: 1, matchTime: 'TBA' },
-  { name: 'Frontier Negotiator', points: 0, matchesPlayed: 1, currentPair: 3, matchTime: 'TBA' },
+  { name: 'Frontier Negotiator', points: 0, matchesPlayed: 1, currentPair: 3, matchTime: 'Crossfire | Scrambler | Razors Edge' },
   { name: 'Two Eggs, One Kol, and a Pure Eradica', points: 0, matchesPlayed: 1,  currentPair: 1, matchTime: 'TBA' },
   { name: 'ISS', points: 0, matchesPlayed: 1, currentPair: 5, matchTime: 'TBA' },
   { name: 'LirTnolikSimcler2xdd', points: 3, matchesPlayed: 1, currentPair: 2, matchTime: 'TBA' },
-  { name: 'The Nameless Ones', points: 3, matchesPlayed: 1, currentPair: 3, matchTime: 'TBA' },
+  { name: 'The Nameless Ones', points: 3, matchesPlayed: 1, currentPair: 3, matchTime: 'Crossfire | Scrambler | Razors Edge' },
   { name: 'Suneaters', points: 3, matchesPlayed: 1, currentPair: 4, matchTime: 'TBA' },
   { name: 'Panda', points: 3, matchesPlayed: 1, currentPair: 4, matchTime: 'TBA' },
   { name: 'Loong', points: 0, matchesPlayed: 1, currentPair: 5, matchTime: 'TBA' },

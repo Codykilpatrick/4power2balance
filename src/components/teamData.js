efc4437a417ad77012643f7184904b97930c446e
aquasaphir <aqua995.ste@gmail.com>
2024-11-15 21:14:56 +0100

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,14 +1,14 @@
 // src/resultsData.js
 const teamData = [
-  { name: 'Bankstreet Boys', points: 3, matchesPlayed: 1, currentPair: 2, matchTime: 'TBA' },
+  { name: 'Bankstreet Boys', points: 3, matchesPlayed: 1, currentPair: 2, matchTime: 'LirtnolikSimcler2xdd won' },
   { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 1, currentPair: 1, matchTime: 'TBA' },
-  { name: 'Frontier Negotiator', points: 0, matchesPlayed: 1, currentPair: 3, matchTime: 'Crossfire | Scrambler | Razors Edge' },
+  { name: 'Frontier Negotiator', points: 0, matchesPlayed: 1, currentPair: 3, matchTime: 'Crossfire | Scrambler | Razors Edge | 19:30 CET' },
   { name: 'Two Eggs, One Kol, and a Pure Eradica', points: 0, matchesPlayed: 1,  currentPair: 1, matchTime: 'TBA' },
   { name: 'ISS', points: 0, matchesPlayed: 1, currentPair: 5, matchTime: 'TBA' },
-  { name: 'LirTnolikSimcler2xdd', points: 3, matchesPlayed: 1, currentPair: 2, matchTime: 'TBA' },
-  { name: 'The Nameless Ones', points: 3, matchesPlayed: 1, currentPair: 3, matchTime: 'Crossfire | Scrambler | Razors Edge' },
-  { name: 'Suneaters', points: 3, matchesPlayed: 1, currentPair: 4, matchTime: 'TBA' },
-  { name: 'Panda', points: 3, matchesPlayed: 1, currentPair: 4, matchTime: 'TBA' },
+  { name: 'LirTnolikSimcler2xdd', points: 3, matchesPlayed: 1, currentPair: 2, matchTime: 'LirtnolikSimcler2xdd won' },
+  { name: 'The Nameless Ones', points: 3, matchesPlayed: 1, currentPair: 3, matchTime: 'Crossfire | Scrambler | Razors Edge | 19:30 CET' },
+  { name: 'Suneaters', points: 3, matchesPlayed: 1, currentPair: 4, matchTime: 'Scrambler | Crossfire | Foreign Invasion' },
+  { name: 'Panda', points: 3, matchesPlayed: 1, currentPair: 4, matchTime: 'Scrambler | Crossfire | Foreign Invasion' },
   { name: 'Loong', points: 0, matchesPlayed: 1, currentPair: 5, matchTime: 'TBA' },
 ];
 

3bc7362d03147afe2f2cf0f0edb8f0b9407f3e39
aquasaphir <aqua995.ste@gmail.com>
2024-11-17 23:28:47 +0100

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -3,7 +3,7 @@ const teamData = [
   { name: 'Bankstreet Boys', points: 3, matchesPlayed: 2, currentPair: 2, matchTime: 'LirtnolikSimcler2xdd won' },
   { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 2, currentPair: 1, matchTime: 'two eggs, one kol and a pure eradica won' },
   { name: 'Frontier Negotiator', points: 0, matchesPlayed: 2, currentPair: 3, matchTime: 'The nameless ones won' },
-  { name: 'two eggs, one kol and a pure eradica', points: 0, matchesPlayed: 2,  currentPair: 1, matchTime: 'two eggs, one kol and a pure eradica won' },
+  { name: 'two eggs, one kol and a pure eradica', points: 3, matchesPlayed: 2,  currentPair: 1, matchTime: 'two eggs, one kol and a pure eradica won' },
   { name: 'ISS', points: 0, matchesPlayed: 1, currentPair: 5, matchTime: 'TBA' },
   { name: 'LirTnolikSimcler2xdd', points: 6, matchesPlayed: 2, currentPair: 2, matchTime: 'LirtnolikSimcler2xdd won' },
   { name: 'The nameless ones', points: 6, matchesPlayed: 2, currentPair: 3, matchTime: 'The nameless ones won' },

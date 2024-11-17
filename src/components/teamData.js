35841e09e2cb70c73eeae8438c46b6c139c276d0
aquasaphir <aqua995.ste@gmail.com>
2024-11-17 19:19:18 +0100

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,9 +1,9 @@
 // src/resultsData.js
 const teamData = [
   { name: 'Bankstreet Boys', points: 3, matchesPlayed: 2, currentPair: 2, matchTime: 'LirtnolikSimcler2xdd won' },
-  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 1, currentPair: 1, matchTime: 'TBA' },
+  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 2, currentPair: 1, matchTime: 'two eggs, one kol and a pure eradica won' },
   { name: 'Frontier Negotiator', points: 0, matchesPlayed: 1, currentPair: 3, matchTime: 'Crossfire | Scrambler | Razors Edge | 17.11.2024 19:30 CET' },
-  { name: 'Two Eggs, One Kol, and a Pure Eradica', points: 0, matchesPlayed: 1,  currentPair: 1, matchTime: 'TBA' },
+  { name: 'two eggs, one kol and a pure eradica', points: 0, matchesPlayed: 1,  currentPair: 1, matchTime: 'two eggs, one kol and a pure eradica' },
   { name: 'ISS', points: 0, matchesPlayed: 1, currentPair: 5, matchTime: 'TBA' },
   { name: 'LirTnolikSimcler2xdd', points: 6, matchesPlayed: 2, currentPair: 2, matchTime: 'LirtnolikSimcler2xdd won' },
   { name: 'The Nameless Ones', points: 3, matchesPlayed: 1, currentPair: 3, matchTime: 'Crossfire | Scrambler | Razors Edge | 17.11.2024 19:30 CET' },

2fdb57e2e4505f6ba423d5089f80b722543c3a07
aquasaphir <aqua995.ste@gmail.com>
2024-11-25 20:01:43 +0100

    UpdateRound3 and Ordering by size teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,15 +1,15 @@
 // src/resultsData.js
 const teamData = [
-  { name: 'Bankstreet Boys', points: 3, matchesPlayed: 2, currentPair: 2, matchTime: 'LirtnolikSimcler2xdd won' },
-  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 2, currentPair: 1, matchTime: 'two eggs, one kol and a pure eradica won' },
-  { name: 'Frontier Negotiator', points: 0, matchesPlayed: 2, currentPair: 3, matchTime: 'The nameless ones won' },
-  { name: 'two eggs, one kol and a pure eradica', points: 3, matchesPlayed: 2,  currentPair: 1, matchTime: 'two eggs, one kol and a pure eradica won' },
-  { name: 'ISS', points: 0, matchesPlayed: 1, currentPair: 5, matchTime: 'TBA' },
-  { name: 'LirTnolikSimcler2xdd', points: 6, matchesPlayed: 2, currentPair: 2, matchTime: 'LirtnolikSimcler2xdd won' },
-  { name: 'The nameless ones', points: 6, matchesPlayed: 2, currentPair: 3, matchTime: 'The nameless ones won' },
-  { name: 'Suneaters', points: 3, matchesPlayed: 1, currentPair: 4, matchTime: 'Scrambler | Crossfire | Foreign Invasion' },
-  { name: 'Panda', points: 3, matchesPlayed: 1, currentPair: 4, matchTime: 'Scrambler | Crossfire | Foreign Invasion' },
-  { name: 'Loong', points: 0, matchesPlayed: 1, currentPair: 5, matchTime: 'TBA' },
+  { name: 'two eggs, one kol and a pure eradica', points: 3, matchesPlayed: 2,  currentPair: 3, matchTime: 'TBA' },
+  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 2, currentPair: 5, matchTime: 'TBA' },
+  { name: 'LirTnolikSimcler2xdd', points: 6, matchesPlayed: 2, currentPair: 2, matchTime: 'TBA' },
+  { name: 'Frontier Negotiator', points: 0, matchesPlayed: 2, currentPair: 5, matchTime: 'TBA' },
+  { name: 'The nameless ones', points: 6, matchesPlayed: 2, currentPair: 1, matchTime: 'TBA' },
+  { name: 'Bankstreet Boys', points: 3, matchesPlayed: 2, currentPair: 4, matchTime: 'TBA' },
+  { name: 'Suneaters', points: 6, matchesPlayed: 2, currentPair: 1, matchTime: 'TBA' },
+  { name: 'Panda', points: 3, matchesPlayed: 2, currentPair: 2, matchTime: 'TBA' },
+  { name: 'Loong', points: 0, matchesPlayed: 2, currentPair: 3, matchTime: 'TBA' },
+  { name: 'ISS', points: 0, matchesPlayed: 2, currentPair: 4, matchTime: 'TBA' },
 ];
 
 export default teamData;

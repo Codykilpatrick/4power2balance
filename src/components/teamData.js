bf7bf9691482ea829063295a348567e60975c5c0
aquasaphir <aqua995.ste@gmail.com>
2024-11-25 20:05:22 +0100

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -8,7 +8,7 @@ const teamData = [
   { name: 'Bankstreet Boys', points: 3, matchesPlayed: 2, currentPair: 4, matchTime: 'TBA' },
   { name: 'Suneaters', points: 6, matchesPlayed: 2, currentPair: 1, matchTime: 'TBA' },
   { name: 'Panda', points: 3, matchesPlayed: 2, currentPair: 2, matchTime: 'TBA' },
-  { name: 'Loong', points: 0, matchesPlayed: 2, currentPair: 3, matchTime: 'TBA' },
+  { name: 'Loong', points: 3, matchesPlayed: 2, currentPair: 3, matchTime: 'TBA' },
   { name: 'ISS', points: 0, matchesPlayed: 2, currentPair: 4, matchTime: 'TBA' },
 ];
 

b29d9ad6a35f84555aa6726ca70891d7e8d0a022
aquasaphir <aqua995.ste@gmail.com>
2024-11-27 23:35:49 +0100

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -6,8 +6,8 @@ const teamData = [
   { name: 'Frontier Negotiator', points: 0, matchesPlayed: 2, currentPair: 5, matchTime: 'Crossfire | Transtav | Razors Edge | Sunday 13:00 CET' },
   { name: 'The nameless ones', points: 6, matchesPlayed: 2, currentPair: 1, matchTime: 'Scrambler | Crossfire | Foreign Invasion | Friday 21:00 CET' },
   { name: 'Bankstreet Boys', points: 3, matchesPlayed: 2, currentPair: 4, matchTime: 'TBA' },
-  { name: 'Suneaters', points: 6, matchesPlayed: 2, currentPair: 1, matchTime: 'TBA' },
-  { name: 'Panda', points: 3, matchesPlayed: 2, currentPair: 2, matchTime: 'Scrambler | Crossfire | Foreign Invasion | Friday 21:00 CET' },
+  { name: 'Suneaters', points: 6, matchesPlayed: 2, currentPair: 1, matchTime: 'Scrambler | Crossfire | Foreign Invasion | Friday 21:00 CET' },
+  { name: 'Panda', points: 3, matchesPlayed: 2, currentPair: 2, matchTime: 'TBA' },
   { name: 'Loong', points: 3, matchesPlayed: 2, currentPair: 3, matchTime: 'TBA' },
   { name: 'ISS', points: 0, matchesPlayed: 2, currentPair: 4, matchTime: 'TBA' },
 ];

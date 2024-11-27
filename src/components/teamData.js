1e6b51e126ca2a77caec8c14f0daa70d4df77563
aquasaphir <aqua995.ste@gmail.com>
2024-11-27 23:07:08 +0100

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,11 +1,11 @@
 // src/resultsData.js
 const teamData = [
   { name: 'two eggs, one kol and a pure eradica', points: 3, matchesPlayed: 2,  currentPair: 3, matchTime: 'TBA' },
-  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 2, currentPair: 5, matchTime: 'TBA' },
+  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 2, currentPair: 5, matchTime: 'Crossfire | Transtav | Razors Edge | Sunday 13:00 CET' },
   { name: 'LirTnolikSimcler2xdd', points: 6, matchesPlayed: 2, currentPair: 2, matchTime: 'TBA' },
-  { name: 'Frontier Negotiator', points: 0, matchesPlayed: 2, currentPair: 5, matchTime: 'TBA' },
-  { name: 'The nameless ones', points: 6, matchesPlayed: 2, currentPair: 1, matchTime: 'Scrambler | Crossfire | Foreign Invasion' },
-  { name: 'Bankstreet Boys', points: 3, matchesPlayed: 2, currentPair: 4, matchTime: 'Scrambler | Crossfire | Foreign Invasion' },
+  { name: 'Frontier Negotiator', points: 0, matchesPlayed: 2, currentPair: 5, matchTime: 'Crossfire | Transtav | Razors Edge | Sunday 13:00 CET' },
+  { name: 'The nameless ones', points: 6, matchesPlayed: 2, currentPair: 1, matchTime: 'Scrambler | Crossfire | Foreign Invasion | Friday 21:00 CET' },
+  { name: 'Bankstreet Boys', points: 3, matchesPlayed: 2, currentPair: 4, matchTime: 'Scrambler | Crossfire | Foreign Invasion | Friday 21:00 CET' },
   { name: 'Suneaters', points: 6, matchesPlayed: 2, currentPair: 1, matchTime: 'TBA' },
   { name: 'Panda', points: 3, matchesPlayed: 2, currentPair: 2, matchTime: 'TBA' },
   { name: 'Loong', points: 3, matchesPlayed: 2, currentPair: 3, matchTime: 'TBA' },

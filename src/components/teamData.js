8e6265e9b3d4f3e1b3c0bff2120a2d80e9bafab6
aquasaphir <aqua995.ste@gmail.com>
2024-11-30 21:19:03 +0100

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -2,12 +2,12 @@
 const teamData = [
   { name: 'two eggs, one kol and a pure eradica', points: 3, matchesPlayed: 2,  currentPair: 3, matchTime: 'Loong won' },
   { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 2, currentPair: 5, matchTime: 'Crossfire | Transtav | Razors Edge | Sunday 13:00 CET' },
-  { name: 'LirTnolikSimcler2xdd', points: 6, matchesPlayed: 2, currentPair: 2, matchTime: 'Scrambler | Crossfire | Transtav' },
+  { name: 'LirTnolikSimcler2xdd', points: 6, matchesPlayed: 2, currentPair: 2, matchTime: 'LirTnolikSimcler2xdd won' },
   { name: 'Frontier Negotiator', points: 0, matchesPlayed: 2, currentPair: 5, matchTime: 'Crossfire | Transtav | Razors Edge | Sunday 13:00 CET' },
   { name: 'The nameless ones', points: 6, matchesPlayed: 2, currentPair: 1, matchTime: 'The nameless ones won' },
   { name: 'Bankstreet Boys', points: 3, matchesPlayed: 2, currentPair: 4, matchTime: 'Scrambler | Crossfire | Transtav | 20:00 CET' },
   { name: 'Suneaters', points: 6, matchesPlayed: 2, currentPair: 1, matchTime: 'The nameless ones won' },
-  { name: 'Panda', points: 3, matchesPlayed: 2, currentPair: 2, matchTime: 'Scrambler | Crossfire | Transtav' },
+  { name: 'Panda', points: 3, matchesPlayed: 2, currentPair: 2, matchTime: 'LirTnolikSimcler2xdd won' },
   { name: 'Loong', points: 3, matchesPlayed: 2, currentPair: 3, matchTime: 'Loong won' },
   { name: 'ISS', points: 0, matchesPlayed: 2, currentPair: 4, matchTime: 'Scrambler | Crossfire | Transtav | 20:00 CET' },
 ];

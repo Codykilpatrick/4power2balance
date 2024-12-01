abfea1c2db2c5648f5e37e52d22e0dbb958b3488
aquasaphir <aqua995.ste@gmail.com>
2024-12-01 22:06:50 +0100

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -5,11 +5,11 @@ const teamData = [
   { name: 'LirTnolikSimcler2xdd', points: 9, matchesPlayed: 3, currentPair: 2, matchTime: 'LirTnolikSimcler2xdd won' },
   { name: 'Frontier Negotiator', points: 3, matchesPlayed: 3, currentPair: 5, matchTime: 'Frontier Negotiator won' },
   { name: 'The nameless ones', points: 9, matchesPlayed: 3, currentPair: 1, matchTime: 'The nameless ones won' },
-  { name: 'Bankstreet Boys', points: 3, matchesPlayed: 2, currentPair: 4, matchTime: 'Scrambler | Crossfire | Transtav | 20:00 CET' },
+  { name: 'Bankstreet Boys', points: 3, matchesPlayed: 2, currentPair: 4, matchTime: 'Bankstreet Boys won' },
   { name: 'Suneaters', points: 6, matchesPlayed: 3, currentPair: 1, matchTime: 'The nameless ones won' },
   { name: 'Panda', points: 3, matchesPlayed: 3, currentPair: 2, matchTime: 'LirTnolikSimcler2xdd won' },
   { name: 'Loong', points: 6, matchesPlayed: 3, currentPair: 3, matchTime: 'Loong won' },
-  { name: 'ISS', points: 0, matchesPlayed: 2, currentPair: 4, matchTime: 'Scrambler | Crossfire | Transtav | 20:00 CET' },
+  { name: 'ISS', points: 0, matchesPlayed: 2, currentPair: 4, matchTime: 'Bankstreet Boys won' },
 ];
 
 export default teamData;

3bee585b36b896488b6f0bd65b0c01c0d067486a
aquasaphir <aqua995.ste@gmail.com>
2024-12-01 16:08:11 +0100

    Update standings teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,14 +1,14 @@
 // src/resultsData.js
 const teamData = [
-  { name: 'two eggs, one kol and a pure eradica', points: 3, matchesPlayed: 2,  currentPair: 3, matchTime: 'Loong won' },
-  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 2, currentPair: 5, matchTime: 'Frontier Negotiator won' },
-  { name: 'LirTnolikSimcler2xdd', points: 6, matchesPlayed: 2, currentPair: 2, matchTime: 'LirTnolikSimcler2xdd won' },
-  { name: 'Frontier Negotiator', points: 0, matchesPlayed: 2, currentPair: 5, matchTime: 'Frontier Negotiator won' },
-  { name: 'The nameless ones', points: 6, matchesPlayed: 2, currentPair: 1, matchTime: 'The nameless ones won' },
+  { name: 'two eggs, one kol and a pure eradica', points: 3, matchesPlayed: 3,  currentPair: 3, matchTime: 'Loong won' },
+  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 3, currentPair: 5, matchTime: 'Frontier Negotiator won' },
+  { name: 'LirTnolikSimcler2xdd', points: 9, matchesPlayed: 3, currentPair: 2, matchTime: 'LirTnolikSimcler2xdd won' },
+  { name: 'Frontier Negotiator', points: 3, matchesPlayed: 3, currentPair: 5, matchTime: 'Frontier Negotiator won' },
+  { name: 'The nameless ones', points: 9, matchesPlayed: 3, currentPair: 1, matchTime: 'The nameless ones won' },
   { name: 'Bankstreet Boys', points: 3, matchesPlayed: 2, currentPair: 4, matchTime: 'Scrambler | Crossfire | Transtav | 20:00 CET' },
-  { name: 'Suneaters', points: 6, matchesPlayed: 2, currentPair: 1, matchTime: 'The nameless ones won' },
-  { name: 'Panda', points: 3, matchesPlayed: 2, currentPair: 2, matchTime: 'LirTnolikSimcler2xdd won' },
-  { name: 'Loong', points: 3, matchesPlayed: 2, currentPair: 3, matchTime: 'Loong won' },
+  { name: 'Suneaters', points: 6, matchesPlayed: 3, currentPair: 1, matchTime: 'The nameless ones won' },
+  { name: 'Panda', points: 3, matchesPlayed: 3, currentPair: 2, matchTime: 'LirTnolikSimcler2xdd won' },
+  { name: 'Loong', points: 6, matchesPlayed: 3, currentPair: 3, matchTime: 'Loong won' },
   { name: 'ISS', points: 0, matchesPlayed: 2, currentPair: 4, matchTime: 'Scrambler | Crossfire | Transtav | 20:00 CET' },
 ];
 

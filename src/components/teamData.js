331a6561c73baa9422c71e7ffd54f0950d2a2d6a
aquasaphir <aqua995.ste@gmail.com>
2024-12-06 16:43:24 +0100

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,9 +1,9 @@
 // src/resultsData.js
 const teamData = [
-  { name: 'two eggs, one kol and a pure eradica', points: 3, matchesPlayed: 3,  currentPair: 4, matchTime: 'TBA' },
+  { name: 'two eggs, one kol and a pure eradica', points: 3, matchesPlayed: 3,  currentPair: 4, matchTime: 'Transtav | Scrambler | Crossfire | 08 DEC 20:30 CET' },
   { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 3, currentPair: 5, matchTime: 'Razors Edge | Scrambler | Crossfire | 08 DEC' },
   { name: 'LirTnolikSimcler2xdd', points: 9, matchesPlayed: 3, currentPair: 1, matchTime: 'TBA' },
-  { name: 'Frontier Negotiator', points: 3, matchesPlayed: 3, currentPair: 4, matchTime: 'TBA' },
+  { name: 'Frontier Negotiator', points: 3, matchesPlayed: 3, currentPair: 4, matchTime: 'Transtav | Scrambler | Crossfire | 08 DEC 20:30 CET' },
   { name: 'The nameless ones', points: 9, matchesPlayed: 3, currentPair: 1, matchTime: 'Razors Edge | Scrambler | Crossfire | 08 DEC' },
   { name: 'Bankstreet Boys', points: 6, matchesPlayed: 3, currentPair: 3, matchTime: 'TBA' },
   { name: 'Suneaters', points: 6, matchesPlayed: 3, currentPair: 2, matchTime: 'TBA' },

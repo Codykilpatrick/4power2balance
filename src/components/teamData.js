9b296a2a6c4a674e1e3e399302c8b1900b29bbdb
aquasaphir <aqua995.ste@gmail.com>
2024-12-07 15:40:34 +0100

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,13 +1,13 @@
 // src/resultsData.js
 const teamData = [
   { name: 'two eggs, one kol and a pure eradica', points: 3, matchesPlayed: 3,  currentPair: 4, matchTime: 'Transtav | Scrambler | Crossfire | 08 DEC 20:30 CET' },
-  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 3, currentPair: 5, matchTime: 'Razors Edge | Scrambler | Crossfire | 08 DEC' },
-  { name: 'LirTnolikSimcler2xdd', points: 9, matchesPlayed: 3, currentPair: 1, matchTime: 'TBA' },
+  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 3, currentPair: 5, matchTime: 'TBA' },
+  { name: 'LirTnolikSimcler2xdd', points: 9, matchesPlayed: 3, currentPair: 1, matchTime: 'Razors Edge | Scrambler | Crossfire | 08 DEC' },
   { name: 'Frontier Negotiator', points: 3, matchesPlayed: 3, currentPair: 4, matchTime: 'Transtav | Scrambler | Crossfire | 08 DEC 20:30 CET' },
   { name: 'The nameless ones', points: 9, matchesPlayed: 3, currentPair: 1, matchTime: 'Razors Edge | Scrambler | Crossfire | 08 DEC' },
-  { name: 'Bankstreet Boys', points: 6, matchesPlayed: 3, currentPair: 3, matchTime: 'TBA' },
+  { name: 'Bankstreet Boys', points: 6, matchesPlayed: 3, currentPair: 3, matchTime: '1:1 | Game3 08 DEC' },
   { name: 'Suneaters', points: 6, matchesPlayed: 3, currentPair: 2, matchTime: 'TBA' },
-  { name: 'Panda', points: 3, matchesPlayed: 3, currentPair: 3, matchTime: 'TBA' },
+  { name: 'Panda', points: 3, matchesPlayed: 3, currentPair: 3, matchTime: '1:1 | Game3 08 DEC' },
   { name: 'Loong', points: 6, matchesPlayed: 3, currentPair: 2, matchTime: 'TBA' },
   { name: 'ISS', points: 0, matchesPlayed: 3, currentPair: 5, matchTime: 'TBA' },
 ];

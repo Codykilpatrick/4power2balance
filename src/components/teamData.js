4f4b8efa5710b452e78d3b86376cffca239e4bee
aquasaphir <aqua995.ste@gmail.com>
2024-12-10 17:50:02 +0100

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -2,9 +2,9 @@
 const teamData = [
   { name: 'two eggs, one kol and a pure eradica', points: 3, matchesPlayed: 3,  currentPair: 4, matchTime: 'two eggs, one kol and a pure eradica won' },
   { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 3, currentPair: 5, matchTime: 'TBA' },
-  { name: 'LirTnolikSimcler2xdd', points: 9, matchesPlayed: 3, currentPair: 1, matchTime: '1:1 | Razors Edge' },
+  { name: 'LirTnolikSimcler2xdd', points: 9, matchesPlayed: 3, currentPair: 1, matchTime: '1:1 | Razors Edge | DEC 14 19:00' },
   { name: 'Frontier Negotiator', points: 3, matchesPlayed: 3, currentPair: 4, matchTime: 'two eggs, one kol and a pure eradica won' },
-  { name: 'The nameless ones', points: 9, matchesPlayed: 3, currentPair: 1, matchTime: '1:1 | Razors Edge' },
+  { name: 'The nameless ones', points: 9, matchesPlayed: 3, currentPair: 1, matchTime: '1:1 | Razors Edge | DEC 14 19:00' },
   { name: 'Bankstreet Boys', points: 9, matchesPlayed: 4, currentPair: 3, matchTime: 'Bankstreet Boys won' },
   { name: 'Suneaters', points: 6, matchesPlayed: 3, currentPair: 2, matchTime: 'TBA' },
   { name: 'Panda', points: 3, matchesPlayed: 3, currentPair: 3, matchTime: 'Bankstreet Boys' },

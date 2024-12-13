427fe31d9e7ede5b3a8117232963cf6df6f922be
aquasaphir <aqua995.ste@gmail.com>
2024-12-13 22:38:13 +0100

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -6,9 +6,9 @@ const teamData = [
   { name: 'Frontier Negotiator', points: 3, matchesPlayed: 3, currentPair: 4, matchTime: 'two eggs, one kol and a pure eradica won' },
   { name: 'The nameless ones', points: 9, matchesPlayed: 3, currentPair: 1, matchTime: '1:1 | Razors Edge | Sunday DEC 14 19:00 CET' },
   { name: 'Bankstreet Boys', points: 9, matchesPlayed: 4, currentPair: 3, matchTime: 'Bankstreet Boys won' },
-  { name: 'Suneaters', points: 6, matchesPlayed: 3, currentPair: 2, matchTime: 'Foreign Invasion | Razors Edge | Scrambler | Sunday DEC 15 3:00 CET' },
+  { name: 'Suneaters', points: 6, matchesPlayed: 3, currentPair: 2, matchTime: 'Foreign Invasion | Razors Edge | Scrambler | Sunday DEC 14 3:00 CET' },
   { name: 'Panda', points: 3, matchesPlayed: 3, currentPair: 3, matchTime: 'Bankstreet Boys' },
-  { name: 'Loong', points: 6, matchesPlayed: 3, currentPair: 2, matchTime: 'Foreign Invasion | Razors Edge | Scrambler | Sunday DEC 15 3:00 CET' },
+  { name: 'Loong', points: 6, matchesPlayed: 3, currentPair: 2, matchTime: 'Foreign Invasion | Razors Edge | Scrambler | Sunday DEC 14 3:00 CET' },
   { name: 'ISS', points: 0, matchesPlayed: 3, currentPair: 5, matchTime: 'Sunday DEC 15' },
 ];
 

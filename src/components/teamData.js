2f5ef6e8f4f29ac6084d98cccf074eff09674b04
aquasaphir <aqua995.ste@gmail.com>
2024-12-15 11:24:09 +0100

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,14 +1,14 @@
 // src/resultsData.js
 const teamData = [
-  { name: 'two eggs, one kol and a pure eradica', points: 3, matchesPlayed: 3,  currentPair: 4, matchTime: 'two eggs, one kol and a pure eradica won' },
+  { name: 'two eggs, one kol and a pure eradica', points: 6, matchesPlayed: 4,  currentPair: 4, matchTime: 'two eggs, one kol and a pure eradica won' },
   { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 3, currentPair: 5, matchTime: 'Sunday DEC 15' },
-  { name: 'LirTnolikSimcler2xdd', points: 9, matchesPlayed: 3, currentPair: 1, matchTime: '1:1 | Razors Edge | DEC 14 19:00 CET' },
-  { name: 'Frontier Negotiator', points: 3, matchesPlayed: 3, currentPair: 4, matchTime: 'two eggs, one kol and a pure eradica won' },
-  { name: 'The nameless ones', points: 9, matchesPlayed: 3, currentPair: 1, matchTime: '1:1 | Razors Edge | Sunday DEC 14 19:00 CET' },
+  { name: 'LirTnolikSimcler2xdd', points: 12, matchesPlayed: 4, currentPair: 1, matchTime: 'LirTnolikSimcler2xdd won' },
+  { name: 'Frontier Negotiator', points: 3, matchesPlayed: 4, currentPair: 4, matchTime: 'two eggs, one kol and a pure eradica won' },
+  { name: 'The nameless ones', points: 9, matchesPlayed: 4, currentPair: 1, matchTime: 'LirTnolikSimcler2xdd won' },
   { name: 'Bankstreet Boys', points: 9, matchesPlayed: 4, currentPair: 3, matchTime: 'Bankstreet Boys won' },
-  { name: 'Suneaters', points: 6, matchesPlayed: 3, currentPair: 2, matchTime: 'Foreign Invasion | Razors Edge | Scrambler | Sunday DEC 14 3:00 CET' },
-  { name: 'Panda', points: 3, matchesPlayed: 3, currentPair: 3, matchTime: 'Bankstreet Boys' },
-  { name: 'Loong', points: 6, matchesPlayed: 3, currentPair: 2, matchTime: 'Foreign Invasion | Razors Edge | Scrambler | Sunday DEC 14 3:00 CET' },
+  { name: 'Suneaters', points: 6, matchesPlayed: 4, currentPair: 2, matchTime: 'Loong won' },
+  { name: 'Panda', points: 3, matchesPlayed: 4, currentPair: 3, matchTime: 'Bankstreet Boys won' },
+  { name: 'Loong', points: 9, matchesPlayed: 4, currentPair: 2, matchTime: 'Loong won' },
   { name: 'ISS', points: 0, matchesPlayed: 3, currentPair: 5, matchTime: 'Sunday DEC 15' },
 ];
 

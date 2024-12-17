4d19b3403c7bdac0652ca2e1a4860c8c3ee285e3
aquasaphir <aqua995.ste@gmail.com>
2024-12-17 20:44:47 +0100

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,7 +1,7 @@
 // src/resultsData.js
 const teamData = [
   { name: 'two eggs, one kol and a pure eradica', points: 6, matchesPlayed: 4,  currentPair: 4, matchTime: 'two eggs, one kol and a pure eradica won' },
-  { name: 'I`d sin for that solar empire', points: 0, matchesPlayed: 3, currentPair: 5, matchTime: 'I`d sin for that solar empire won' },
+  { name: 'I`d sin for that solar empire', points: 3, matchesPlayed: 4, currentPair: 5, matchTime: 'I`d sin for that solar empire won' },
   { name: 'LirTnolikSimcler2xdd', points: 12, matchesPlayed: 4, currentPair: 1, matchTime: 'LirTnolikSimcler2xdd won' },
   { name: 'Frontier Negotiator', points: 3, matchesPlayed: 4, currentPair: 4, matchTime: 'two eggs, one kol and a pure eradica won' },
   { name: 'The nameless ones', points: 9, matchesPlayed: 4, currentPair: 1, matchTime: 'LirTnolikSimcler2xdd won' },
@@ -9,7 +9,7 @@ const teamData = [
   { name: 'Suneaters', points: 6, matchesPlayed: 4, currentPair: 2, matchTime: 'Loong won' },
   { name: 'Panda', points: 3, matchesPlayed: 4, currentPair: 3, matchTime: 'Bankstreet Boys won' },
   { name: 'Loong', points: 9, matchesPlayed: 4, currentPair: 2, matchTime: 'Loong won' },
-  { name: 'ISS', points: 0, matchesPlayed: 3, currentPair: 5, matchTime: 'I`d sin for that solar empire won' },
+  { name: 'ISS', points: 0, matchesPlayed: 4, currentPair: 5, matchTime: 'I`d sin for that solar empire won' },
 ];
 
 export default teamData;

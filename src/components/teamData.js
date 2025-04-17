074a00c676b191a121527430e436bcbbc5fc705f
aquasaphir <aqua995.ste@gmail.com>
2025-04-17 11:40:07 +0200

    TEAM EP teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,15 +1,15 @@
 // src/resultsData.js
 const teamData = [
-  { name: 'two eggs, one kol and a pure eradica', points: 6, matchesPlayed: 4,  currentPair: 4, matchTime: 'two eggs, one kol and a pure eradica won' },
-  { name: 'I`d sin for that solar empire', points: 3, matchesPlayed: 4, currentPair: 5, matchTime: 'I`d sin for that solar empire won' },
-  { name: 'LirTnolikSimcler2xdd', points: 12, matchesPlayed: 4, currentPair: 1, matchTime: 'LirTnolikSimcler2xdd won' },
-  { name: 'Frontier Negotiator', points: 3, matchesPlayed: 4, currentPair: 4, matchTime: 'two eggs, one kol and a pure eradica won' },
-  { name: 'The nameless ones', points: 9, matchesPlayed: 4, currentPair: 1, matchTime: 'LirTnolikSimcler2xdd won' },
-  { name: 'Bankstreet Boys', points: 9, matchesPlayed: 4, currentPair: 3, matchTime: 'Bankstreet Boys won' },
-  { name: 'Suneaters', points: 6, matchesPlayed: 4, currentPair: 2, matchTime: 'Loong won' },
-  { name: 'Panda', points: 3, matchesPlayed: 4, currentPair: 3, matchTime: 'Bankstreet Boys won' },
-  { name: 'Loong', points: 9, matchesPlayed: 4, currentPair: 2, matchTime: 'Loong won' },
-  { name: 'ISS', points: 0, matchesPlayed: 4, currentPair: 5, matchTime: 'I`d sin for that solar empire won' },
+  { name: 'two eggs, one kol and a pure eradica', EP: 14, points: 6, matchesPlayed: 4,  currentPair: 4, matchTime: 'two eggs, one kol and a pure eradica won' },
+  { name: 'I`d sin for that solar empire', EP: 11, points: 3, matchesPlayed: 4, currentPair: 5, matchTime: 'I`d sin for that solar empire won' },
+  { name: 'LirTnolikSimcler2xdd', EP: 29, points: 12, matchesPlayed: 4, currentPair: 1, matchTime: 'LirTnolikSimcler2xdd won' },
+  { name: 'Frontier Negotiator', EP: 12, points: 3, matchesPlayed: 4, currentPair: 4, matchTime: 'two eggs, one kol and a pure eradica won' },
+  { name: 'The nameless ones', EP: 23, points: 9, matchesPlayed: 4, currentPair: 1, matchTime: 'LirTnolikSimcler2xdd won' },
+  { name: 'Bankstreet Boys', EP: 20, points: 9, matchesPlayed: 4, currentPair: 3, matchTime: 'Bankstreet Boys won' },
+  { name: 'Suneaters', EP: 14, points: 6, matchesPlayed: 4, currentPair: 2, matchTime: 'Loong won' },
+  { name: 'Panda', EP: 12, points: 3, matchesPlayed: 4, currentPair: 3, matchTime: 'Bankstreet Boys won' },
+  { name: 'Loong', EP: 19, points: 9, matchesPlayed: 4, currentPair: 2, matchTime: 'Loong won' },
+  { name: 'ISS', EP: 6, points: 0, matchesPlayed: 4, currentPair: 5, matchTime: 'I`d sin for that solar empire won' },
 ];
 
 export default teamData;

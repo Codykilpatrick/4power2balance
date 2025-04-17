bf64a587e7de0ec96c82a877ff5077d31ff2c099
aquasaphir <aqua995.ste@gmail.com>
2025-04-17 11:56:14 +0200

    Create playerData.js

---
+++ src/components/playerData.js
@@ -0,0 +1,44 @@
+// src/resultsData.js
+
+const playerData = [
+  { name: 'davy', EP: 14, matchesPlayed: 4, wins: 2, currentTeam: 'two eggs, one kol and a pure eradica' },
+  { name: 'bikaruu', EP: 14, matchesPlayed: 4, wins: 2, currentTeam: 'two eggs, one kol and a pure eradica' },
+  { name: 'holo_icy_holo', EP: 14, matchesPlayed: 4, wins: 2, currentTeam: 'two eggs, one kol and a pure eradica' },
+
+  { name: 'Duckeh', EP: 11, matchesPlayed: 4, wins: 1, currentTeam: 'I`d sin for that solar empire' },
+  { name: 'ncc', EP: 11, matchesPlayed: 4, wins: 1, currentTeam: 'I`d sin for that solar empire' },
+
+  { name: 'sinclair2_', EP: 29, matchesPlayed: 4, wins: 4, currentTeam: 'LirTnolikSimcler2xdd' },
+  { name: 'Trolik', EP: 29, matchesPlayed: 4, wins: 4, currentTeam: 'LirTnolikSimcler2xdd' },
+  { name: 'Lear_the_Cat', EP: 29, matchesPlayed: 4, wins: 4, currentTeam: 'LirTnolikSimcler2xdd' },
+
+  { name: 'Air', EP: 12, matchesPlayed: 4, wins: 1, currentTeam: 'Frontier Negotiator' },
+  { name: 'aqua995', EP: 12, matchesPlayed: 4, wins: 1, currentTeam: 'Frontier Negotiator' },
+
+  { name: 'mathematico', EP: 23, matchesPlayed: 4, wins: 3, currentTeam: 'The nameless ones' },
+  { name: 'auqia', EP: 23, matchesPlayed: 4, wins: 3, currentTeam: 'The nameless ones' },
+  { name: 'rebelcapitalist.', EP: 23, matchesPlayed: 4, wins: 3, currentTeam: 'The nameless ones' },
+
+  { name: 'naviomg', EP: 20, matchesPlayed: 4, wins: 3, currentTeam: 'Bankstreet Boys' },
+  { name: 'doucit', EP: 20, matchesPlayed: 4, wins: 3, currentTeam: 'Bankstreet Boys' },
+
+  { name: 'Avezearth', EP: 14, matchesPlayed: 4, wins: 2, currentTeam: 'Suneaters' },
+  { name: 'Koi', EP: 14, matchesPlayed: 4, wins: 2, currentTeam: 'Suneaters' },
+
+  { name: 'fruitwing', EP: 12, matchesPlayed: 4, wins: 1, currentTeam: 'Panda' },
+  { name: 'Fenix盛', EP: 12, matchesPlayed: 4, wins: 1, currentTeam: 'Panda' },
+  { name: '风 暴 领 域 大 神', EP: 12, matchesPlayed: 4, wins: 1, currentTeam: 'Panda' },
+
+  { name: 'CCC小白', EP: 19, matchesPlayed: 4, wins: 3, currentTeam: 'Loong' },
+  { name: 'X-Wing', EP: 19, matchesPlayed: 4, wins: 3, currentTeam: 'Loong' },
+  { name: '黎梦秋年F', EP: 19, matchesPlayed: 4, wins: 3, currentTeam: 'Loong' },
+
+  { name: 'SilasPT', EP: 6, matchesPlayed: 4, wins: 0, currentTeam: 'ISS' },
+  { name: 'pakss', EP: 6, matchesPlayed: 4, wins: 0, currentTeam: 'ISS' },
+  { name: 'kabianmazorca', EP: 6, matchesPlayed: 4, wins: 0, currentTeam: 'ISS' },
+];
+
+export default playerData;
+
+
+export default playerData;

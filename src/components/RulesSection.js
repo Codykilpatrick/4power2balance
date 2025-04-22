67edcbb56f1e8386843340d68c7653263af75f8c
aquasaphir <aqua995.ste@gmail.com>
2025-04-22 15:19:57 +0200

    Team RulesSection.js

--- src/components/RulesSection.js
+++ src/components/RulesSection.js
@@ -37,9 +37,9 @@ function RulesSection() {
       
       <CollapsibleSection title="Tournament Format">
         <ul className="list-disc list-inside space-y-2">
-          <li><strong>Team Setup:</strong> Each team can register 3 players. To join the tournament, you need to register 2 players right away, and the 3rd player can be added later.</li>
+          <li><strong>Team Setup:</strong> Each team can register 4 players. To join the tournament, you need to register 2 players right away, and an additional player can be added later. I don't want to interfere with Team Politics much, but if a player left a team, they will not be able to join any team or earn EP for the tournament placement. If the remaining players decide to recruit a replacement, they are allowed to do this up to two times. This means if 4 player team loses 2 members, the remaining 2 players are allowed to recruit 2 new members to replace the others. If then again, someone leaves the team, the remaining players need to play with an empty spot in their roster. Resulting in a total of 6 players being able to participate for a team. Ideally all teams have their things sorted out and a good stable roster of 3 main players and 1 backUp that can jump in if needed. I hope this ruling gives enough flexibility and still keeps the original identity of teams.</li>
           <li><strong>Swiss System:</strong> Matches will be paired using the Swiss system to ensure balanced matchups.</li>
-          <li><strong>Best of 1 (Bo1) Matches:</strong> 7 maps are available. Each team uses 2 bans, with the final 3 maps being played. The ban format is 1-2-1. Decide at random who gets the first ban.</li>
+          <li><strong>Best of 3 (Bo3) Matches:</strong> 7 maps are available. Each team uses 2 bans, with the final 3 maps being played. The ban format is 1-2-1. Decide at random who gets the first ban.</li>
           <li><strong>Victory Conditions:</strong> Most competitive players prefer playing with victory conditions off, so that&apos;s what we&apos;ll go with.</li>
           <li><strong>Speed:</strong> Speed will be x1.5, with all other individual speed settings set to normal.</li>
           <li><strong>Rotating Planets:</strong> Rotation On</li>
@@ -56,7 +56,7 @@ function RulesSection() {
           <li>Maelstrom 3v3</li>
           <li>Razor&apos;s Edge 3v3</li>
           <li>
-            In Bo1 matches, teams can ban the 3v3 maps if they want to play on 2v2 maps. By the time you reach the Top 8 rounds, you should have registered a third player, or you will have to win two 2v2 maps while banning a 3v3 map.
+            In Bo3 matches, teams can ban the 3v3 maps if they want to play on 2v2 maps. This means a team of 2 players can win the whole tournament.
           </li>
         </ul>
         <p className="mt-4">
@@ -86,25 +86,25 @@ function RulesSection() {
 
       <CollapsibleSection title="Schedule">
         <p>
-          Pairings will be made between 20:00 CET and 23:00 CET on Mondays. Rounds will end on Mondays at 20:00 CET and start at 23:00 CET. Full pairings will be announced on Discord.
+          Pairings will be made between 20:00 CET Sunday and 23:00 CET on Mondays. Rounds will end on Mondays at 20:00 CET and start at 23:00 CET. Full pairings will be announced on Discord and Reddit.
         </p>
         <p className="mt-4">
           Contact your opponent via Discord or Reddit to schedule a match within the week. Official playdays are Saturday and Sunday.
         </p>
         <ul className="list-disc list-inside mt-4 space-y-2">
-          <li>Round 1: 28.10. - 11.11.</li>
-          <li>Round 2: 11.11. - 25.11.</li>
-          <li>Round 3: 25.11. - 2.12.</li>
-          <li>Round 4: 2.12. - 9.12.</li>
+          <li>Round 1: TBA</li>
+          <li>Round 2: TBA</li>
+          <li>Round 3: TBA</li>
+          <li>Round 4: TBA</li>
         </ul>
         <p className="mt-4">
-          Late sign-ups are allowed in Round 1 (if they make the number of teams even) or Round 2 (if the total is divisible by 4). This ensures fair pairings, avoids free wins due to an uneven number of teams, and gives late joiners a chance to participate.
+          Late sign-ups are allowed in Round 1 (if they make the number of teams even). This ensures fair pairings, avoids free wins due to an uneven number of teams, and gives late joiners a chance to participate.
         </p>
       </CollapsibleSection>
 
       <CollapsibleSection title="Casting & Replays">
         <p>
-          Replays will be uploaded to the official Sins2 Discord with the tag &quot;4P2B&quot; in the filename. If you are interested in casting matches, please contact me.
+          Replays will be uploaded to the official Sins2 Discord with the tag &quot;4P2B&quot; in the filename. If you are interested in casting matches, please contact me. I would be happy if I can work with the cast of the 2024 tournament.
         </p>
         <p className="mt-4">
           Some community members offer to create maps with spectator spots, which will be a possibility.

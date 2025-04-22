91ee8ae81d4419632b0ef7be071952967f306d51
aquasaphir <aqua995.ste@gmail.com>
2025-04-22 14:37:53 +0200

    Update Leaderboard.js

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -11,7 +11,7 @@ const Leaderboard = () => {
     <div className="space-y-12 mb-12">
       {/* --- Teams Leaderboard Section --- */}
       <div>
-        <div>
+        <div className="items-center">
           <h2 className="text-3xl font-bold text-ionized-blue border-b-2 border-ionized-blue pb-2 items-center">
             Leaderboard - Teams
           </h2>

a4c676d2f8409345f0604b05835d6d384da0a284
aquasaphir <aqua995.ste@gmail.com>
2025-05-30 22:56:53 +0200

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -2,8 +2,8 @@
 const teamData = [
   { name: 'LirTnolikSimcler2xdd', EP: 29, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 1, matchTime: 'Sunday 15:00 CEST' },
   { name: 'Frontier Negotiator', EP: 12, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 4, matchTime: 'Sunday 15:00 CEST' },
-  { name: 'Fleet CMD', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 4, matchTime: 'Sunday 15:00 CEST' },
   { name: 'The Singer', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 1, matchTime: 'Sunday 15:00 CEST' },
+  { name: 'Fleet CMD', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 4, matchTime: 'Sunday 15:00 CEST' },
   
   { name: 'I`d sin for that solar empire', EP: 11, points: 3, wins: 1, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'I`d sin for that solar empire won' },
   { name: 'two eggs, one kol and a pure eradica', EP: 14, points: 6, wins: 2, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: "done" },

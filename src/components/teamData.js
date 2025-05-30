00e509ba23003b6da2784ffdb387612e9887155c
aquasaphir <aqua995.ste@gmail.com>
2025-05-30 22:40:48 +0200

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,5 +1,10 @@
 // src/resultsData.js
 const teamData = [
+  { name: 'LirTnolikSimcler2xdd', EP: 29, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 1, matchTime: 'Sunday 15:00 CEST' },
+  { name: 'Frontier Negotiator', EP: 12, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 4, matchTime: 'Sunday 15:00 CEST' },
+  { name: 'Fleet CMD', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 4, matchTime: 'Sunday 15:00 CEST' },
+  { name: 'The Singer', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 1, matchTime: 'Sunday 15:00 CEST' },
+  
   { name: 'two eggs, one kol and a pure eradica', EP: 14, points: 6, wins: 2, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: null },
   { name: 'I`d sin for that solar empire', EP: 11, points: 3, wins: 1, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'I`d sin for that solar empire won' },
   { name: 'The nameless ones', EP: 23, points: 9, wins: 3, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'LirTnolikSimcler2xdd won' },
@@ -9,10 +14,7 @@ const teamData = [
   { name: 'Loong', EP: 19, points: 9, wins: 3, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'Loong won' },
   { name: 'ISS', EP: 6, points: 0, wins: 0, matchesPlayed: 3, currentMatchesPlayed: 4, currentPair: null, matchTime: 'I`d sin for that solar empire won' },
 
-  { name: 'LirTnolikSimcler2xdd', EP: 29, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 1, matchTime: 'Sunday 15:00 CEST' },
-  { name: 'Frontier Negotiator', EP: 12, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 4, matchTime: 'Sunday 15:00 CEST' },
-  { name: 'Fleet CMD', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 4, matchTime: 'Sunday 15:00 CEST' },
-  { name: 'The Singer', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 1, matchTime: 'Sunday 15:00 CEST' },
+ 
 ];
 
 export default teamData;

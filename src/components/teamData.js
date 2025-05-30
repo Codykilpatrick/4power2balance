2db0755be9939f7deab66c0f5297b925164afd9b
aquasaphir <aqua995.ste@gmail.com>
2025-05-30 22:38:30 +0200

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,6 +1,6 @@
 // src/resultsData.js
 const teamData = [
-  { name: 'two eggs, one kol and a pure eradica', EP: 14, points: 6, wins: 2, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'two eggs, one kol and a pure eradica won' },
+  { name: 'two eggs, one kol and a pure eradica', EP: 14, points: 6, wins: 2, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: null },
   { name: 'I`d sin for that solar empire', EP: 11, points: 3, wins: 1, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'I`d sin for that solar empire won' },
   { name: 'The nameless ones', EP: 23, points: 9, wins: 3, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'LirTnolikSimcler2xdd won' },
   { name: 'Bankstreet Boys', EP: 20, points: 9, wins: 3, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'Bankstreet Boys won' },

091bea078d178e1b7f59d66067290d2e1605d396
aquasaphir <aqua995.ste@gmail.com>
2025-05-30 22:29:57 +0200

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -2,10 +2,10 @@
 const teamData = [
   { name: 'two eggs, one kol and a pure eradica', EP: 14, points: 6, wins: 2, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'two eggs, one kol and a pure eradica won' },
   { name: 'I`d sin for that solar empire', EP: 11, points: 3, wins: 1, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'I`d sin for that solar empire won' },
-  { name: 'Panda', EP: 12, points: 3, wins: 1, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'Bankstreet Boys won' },
   { name: 'The nameless ones', EP: 23, points: 9, wins: 3, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'LirTnolikSimcler2xdd won' },
   { name: 'Bankstreet Boys', EP: 20, points: 9, wins: 3, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'Bankstreet Boys won' },
   { name: 'Suneaters', EP: 14, points: 6, wins: 2, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'Loong won' },
+  { name: 'Panda', EP: 12, points: 3, wins: 1, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'Bankstreet Boys won' },
   { name: 'Loong', EP: 19, points: 9, wins: 3, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'Loong won' },
   { name: 'ISS', EP: 6, points: 0, wins: 0, matchesPlayed: 3, currentMatchesPlayed: 4, currentPair: null, matchTime: 'I`d sin for that solar empire won' },
 

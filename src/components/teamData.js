a78ceec3a7d5eb491b2fe97f8633ff3a1e7ee562
aquasaphir <aqua995.ste@gmail.com>
2025-07-25 14:04:04 +0200

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,10 +1,10 @@
 // src/resultsData.js
 const teamData = [
-  { name: 'LirTnolikSimcler2xdd', EP: 31, points: 0, wins: 0, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: 2, matchTime: 'Sunday 15:00 CEST' },
-  { name: 'Frontier Negotiator', EP: 17, points: 3, wins: 1, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: 4, matchTime: 'BYE' },
-  { name: 'The Singer', EP: 5, points: 3, wins: 1, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: 1, matchTime: 'Thursday 22:00 BJT' },
-  { name: 'Fleet CMD', EP: 2, points: 0, wins: 0, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: 1, matchTime: 'Thursday 22:00 BJT' },
-  { name: 'TooSlow', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 2, matchTime: 'Sunday 15:00 CEST' },
+  { name: 'LirTnolikSimcler2xdd', EP: 31, points: 0, wins: 0, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Sunday 15:00 CEST' },
+  { name: 'Frontier Negotiator', EP: 42, points: 3, wins: 1, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: null, matchTime: 'BYE' },
+  { name: 'The Singer', EP: 42, points: 3, wins: 1, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Thursday 22:00 BJT' },
+  { name: 'Fleet CMD', EP: 18, points: 0, wins: 0, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Thursday 22:00 BJT' },
+  { name: 'TooSlow', EP: 11, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Sunday 15:00 CEST' },
   
   { name: 'I`d sin for that solar empire', EP: 11, points: 3, wins: 1, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'I`d sin for that solar empire won' },
   { name: 'two eggs, one kol and a pure eradica', EP: 14, points: 6, wins: 2, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: "done" },

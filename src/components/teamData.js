e6ef36a96d1862b4d0e8eabc184ae194dc4805a4
aquasaphir <aqua995.ste@gmail.com>
2025-10-17 23:37:50 +0200

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,12 +1,10 @@
 // src/resultsData.js
 const teamData = [
-  { name: 'LirTnolikSimcler2xdd', EP: 31, points: 0, wins: 0, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Sunday 15:00 CEST' },
-  { name: 'The Singer', EP: 42, points: 3, wins: 1, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Thursday 22:00 BJT' },
-  { name: 'Frontier Negotiator', EP: 42, points: 3, wins: 1, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: 1, matchTime: 'Saturday 21:00 CEST' },
-  { name: 'Fleet CMD', EP: 18, points: 0, wins: 0, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: 2, matchTime: 'Sunday' },
+  { name: 'LirTnolikSimcler2xdd', EP: 31, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Sunday 15:00 CEST' },
+  { name: 'The Singer', EP: 42, points: 3, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Thursday 22:00 BJT' },
   { name: 'TooSlow', EP: 11, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Sunday 15:00 CEST' },
   
-  { name: 'I`d sin for that solar empire', EP: 11, points: 3, wins: 1, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'I`d sin for that solar empire won' },
+  { name: 'I`d sin for that solar empire', EP: 11, points: 3, wins: 0, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'I`d sin for that solar empire won' },
   { name: 'two eggs, one kol and a pure eradica', EP: 14, points: 6, wins: 2, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: "done" },
   { name: 'The nameless ones', EP: 23, points: 9, wins: 3, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'LirTnolikSimcler2xdd won' },
   { name: 'Bankstreet Boys', EP: 20, points: 9, wins: 3, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'Bankstreet Boys won' },
@@ -20,6 +18,8 @@ const teamData = [
   { name: 'SOMA', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 2, matchTime: 'Sunday' },
   { name: 'TheHermits', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 3, matchTime: 'Sunday' },
   { name: 'Rage against the Aliens', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 3, matchTime: 'Sunday' },
+  { name: 'Frontier Negotiator', EP: 42, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 1, matchTime: 'Saturday 21:00 CEST' },
+  { name: 'Fleet CMD', EP: 18, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 2, matchTime: 'Sunday' },
 ];
 
 export default teamData;

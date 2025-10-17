086310d6e01a258e0774b4b339337a47153ca995
aquasaphir <aqua995.ste@gmail.com>
2025-10-17 18:16:47 +0200

    Update RulesSection.js

--- src/components/RulesSection.js
+++ src/components/RulesSection.js
@@ -39,8 +39,8 @@ function RulesSection() {
         <ul className="list-disc list-inside space-y-2">
           <li><strong>Team Setup:</strong> Each Team can register up to 4 players. To join the tournament, you need to register 2 players right away. Additional players can be added later. I don&apos;t want to interfere with Team Politics much, but if a player left a team, they will not be able to join any team or earn EP for the tournament placement. If the remaining players decide to recruit a replacement, they are allowed to do this up to two times. This means if 4 player team loses 2 members, the remaining 2 players are allowed to recruit 2 new members to replace the others. If then again, someone leaves the team, the remaining players need to play with an empty spot in their roster. Resulting in a total of 6 players being able to participate for a team. Ideally all teams have their things sorted out and a good stable roster of 3 main players and 1 backUp that can jump in if needed. I hope this ruling gives enough flexibility and still keeps the original identity of teams.</li>
           <li><strong>Swiss System:</strong> Matches will be paired using the Swiss system to ensure balanced matchups.</li>
-          <li><strong>Best of 3 (Bo3) Matches:</strong> 7 maps are available. Each team uses 2 bans, with the final 3 maps being played. The ban format is 1-2-1. Decide at random who gets the first ban.</li>
-          <li><strong>Victory Conditions:</strong> Most competitive players prefer playing with victory conditions off, so that&apos;s what we&apos;ll go with.</li>
+          <li><strong>Best of 3 (Bo3) Matches:</strong> 9 maps are available. Each team uses 2 bans, with the final 3 maps being played. The ban format is 1-2-2-1. Decide at random who gets the first ban.</li>
+          <li><strong>Victory Conditions:</strong>Factional Victories on</li>
           <li><strong>Speed:</strong> Speed will be x1.5, with all other individual speed settings set to normal.</li>
           <li><strong>Rotating Planets:</strong> Rotation On</li>
         </ul>
@@ -89,13 +89,12 @@ function RulesSection() {
           Pairings will be made between 20:00 CET Sunday and 23:00 CET on Mondays. Rounds will end on Mondays at 20:00 CET and start at 23:00 CET. Full pairings will be announced on Discord and Reddit.
         </p>
         <p className="mt-4">
-          Contact your opponent via Discord or Reddit to schedule a match within the week. Official playdays are Saturday and Sunday. The official time to play a match will be Sunday 15:00 CET. 
+          Contact your opponent via Discord or Reddit to schedule a match within the week. Official playdays are Saturday and Sunday. 
         </p>
         <ul className="list-disc list-inside mt-4 space-y-2">
-          <li>Round 1: 30.05. - 16.06.</li>
-          <li>Round 2: 16.06. - 30.06.</li>
-          <li>Round 3: 30.06. - 14.07.</li>
-          <li>Round 4: 14.07. - 28.07.</li>
+          <li>Round 1: 17.10. - 03.11. </li>
+          <li>Round 2: 03.11. - 17.11.</li>
+          <li>Round 3: 17.11. - 01.12.</li>
         </ul>
         <p className="mt-4">
           Late sign-ups are allowed in Round 1 (if they make the number of teams even). This ensures fair pairings, avoids free wins due to an uneven number of teams, and gives late joiners a chance to participate.
@@ -104,19 +103,16 @@ function RulesSection() {
 
       <CollapsibleSection title="Casting & Replays">
         <p>
-          Replays will be uploaded to the official Sins2 Discord with the tag &quot;4P2B&quot; in the filename. If you are interested in casting matches, please contact me. I would be happy if I can work with the cast of the 2024 tournament.
-        </p>
-        <p className="mt-4">
-          Some community members offer to create maps with spectator spots, which will be a possibility.
+          Replays will be uploaded to the official Sins2 Discord with the tag &quot;4P2B&quot; in the filename. If you are interested in casting matches, please contact me. So far BiovoreBarrage stays as the main caster of 4P2B.
         </p>
       </CollapsibleSection>
 
       <CollapsibleSection title="Interested in Joining?">
         <p>
-          The tournament is meant to be simple, fun, and exciting. If you&apos;re into Sins2 and want to be part of the growing competitive scene, grab two friends and get ready for the action.
+          The tournament is meant to be simple, fun, fair and exciting. If you&apos;re into Sins2 and want to be part of the growing competitive scene, grab two friends and get ready for the action.
         </p>
         <p className="mt-4">
-          To join, provide your Reddit and Discord usernames and choose a team name. It can be anything as long as it adheres to the sub and Discord rules.
+          To join, provide your Reddit and Discord usernames and choose a team name. It can be anything as long as it adheres to the sub and Discord rules. You can contact me via Discord, Youtube or Reddit. My name is aqua995 everywhere.
         </p>
       </CollapsibleSection>
     </section>

877ea7fb207ec42e53c9e15bdce891fc8bf02110
aquasaphir <aqua995.ste@gmail.com>
2025-10-17 23:36:32 +0200

    Update teamData.js

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -2,8 +2,8 @@
 const teamData = [
   { name: 'LirTnolikSimcler2xdd', EP: 31, points: 0, wins: 0, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Sunday 15:00 CEST' },
   { name: 'The Singer', EP: 42, points: 3, wins: 1, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Thursday 22:00 BJT' },
-  { name: 'Frontier Negotiator', EP: 42, points: 3, wins: 1, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: null, matchTime: 'BYE' },
-  { name: 'Fleet CMD', EP: 18, points: 0, wins: 0, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Thursday 22:00 BJT' },
+  { name: 'Frontier Negotiator', EP: 42, points: 3, wins: 1, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: 1, matchTime: 'Saturday 21:00 CEST' },
+  { name: 'Fleet CMD', EP: 18, points: 0, wins: 0, matchesPlayed: 1, currentMatchesPlayed: 0, currentPair: 2, matchTime: 'Sunday' },
   { name: 'TooSlow', EP: 11, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Sunday 15:00 CEST' },
   
   { name: 'I`d sin for that solar empire', EP: 11, points: 3, wins: 1, matchesPlayed: 4, currentMatchesPlayed: 4, currentPair: null, matchTime: 'I`d sin for that solar empire won' },
@@ -16,10 +16,10 @@ const teamData = [
   { name: 'ISS', EP: 6, points: 0, wins: 0, matchesPlayed: 3, currentMatchesPlayed: 4, currentPair: null, matchTime: 'I`d sin for that solar empire won' },
 
  
-  { name: 'Dad FPS & The Dial-Up kid', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Sunday' },
-  { name: 'SOMA', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Sunday' },
-  { name: 'TheHermits', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Sunday' },
-  { name: 'Rage against the Aliens', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: null, matchTime: 'Sunday' },
+  { name: 'Dad FPS & The Dial-Up kid', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 1, matchTime: 'Saturday 21:00 CEST' },
+  { name: 'SOMA', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 2, matchTime: 'Sunday' },
+  { name: 'TheHermits', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 3, matchTime: 'Sunday' },
+  { name: 'Rage against the Aliens', EP: 0, points: 0, wins: 0, matchesPlayed: 0, currentMatchesPlayed: 0, currentPair: 3, matchTime: 'Sunday' },
 ];
 
 export default teamData;

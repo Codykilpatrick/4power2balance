ded0ef78a46c9d1dd4dd805f96ff9507452c560c
aquasaphir <aqua995.ste@gmail.com>
2025-11-11 15:09:46 +0100

    Update playerData.js

--- src/components/playerData.js
+++ src/components/playerData.js
@@ -2,14 +2,14 @@
 
 const playerData = [
   
-  { name: 'Mephissadiablo', EP: 2, matchesPlayed: 0, wins: 0, currentTeam: 'Rage against the Aliens' },
-  { name: 'raaf12', EP: 2, matchesPlayed: 0, wins: 0, currentTeam: 'Rage against the Aliens' },
+  { name: 'Mephissadiablo', EP: 4, matchesPlayed: 0, wins: 0, currentTeam: 'Rage against the Aliens' },
+  { name: 'raaf12', EP: 4, matchesPlayed: 0, wins: 0, currentTeam: 'Rage against the Aliens' },
   
-  { name: 'Ciber_Monke', EP: 5, matchesPlayed: 0, wins: 0, currentTeam: 'TheHermits' },
-  { name: '.bintro', EP: 5, matchesPlayed: 0, wins: 0, currentTeam: 'TheHermits' },
+  { name: 'Ciber_Monke', EP: 7, matchesPlayed: 0, wins: 0, currentTeam: 'TheHermits' },
+  { name: '.bintro', EP: 7, matchesPlayed: 0, wins: 0, currentTeam: 'TheHermits' },
   
-  { name: 'davy', EP: 19, matchesPlayed: 4, wins: 2, currentTeam: 'Dad FPS & The Dial-Up kid' },
-  { name: 'Koi', EP: 19, matchesPlayed: 4, wins: 2, currentTeam: 'Dad FPS & The Dial-Up kid' },
+  { name: 'davy', EP: 24, matchesPlayed: 4, wins: 2, currentTeam: 'Dad FPS & The Dial-Up kid' },
+  { name: 'Koi', EP: 24, matchesPlayed: 4, wins: 2, currentTeam: 'Dad FPS & The Dial-Up kid' },
   
   { name: 'bikaruu', EP: 14, matchesPlayed: 4, wins: 2, currentTeam: 'two eggs, one kol and a pure eradica' },
 
@@ -30,8 +30,8 @@ const playerData = [
   { name: 'doucit', EP: 20, matchesPlayed: 4, wins: 3, currentTeam: 'Bankstreet Boys' },
   { name: 'Crimeskin', EP: 15, matchesPlayed: 3, wins: 3, currentTeam: 'Bankstreet Boys' },
 
-  { name: 'Avezearth', EP: 15, matchesPlayed: 3, wins: 2, currentTeam: 'SOMA' },
-  { name: 'Matheus[BRz]L', EP: 5, matchesPlayed: 0, wins: 0, currentTeam: 'SOMA' },
+  { name: 'Avezearth', EP: 20, matchesPlayed: 3, wins: 2, currentTeam: 'SOMA' },
+  { name: 'Matheus[BRz]L', EP: 10, matchesPlayed: 0, wins: 0, currentTeam: 'SOMA' },
   
   { name: 'Kissblade', EP: 10, matchesPlayed: 3, wins: 1, currentTeam: 'Suneaters' },
   { name: 'Zergrusher/L', EP: 3, matchesPlayed: 1, wins: 0, currentTeam: 'Suneaters' },
@@ -45,8 +45,8 @@ const playerData = [
   { name: 'lagenaz', EP: 42, matchesPlayed: 0, wins: 0, currentTeam: 'The Singer' },
   { name: 'UED', EP: 42, matchesPlayed: 0, wins: 0, currentTeam: 'The Singer' },  
 
-  { name: 'dannyvye.com', EP: 20, matchesPlayed: 0, wins: 0, currentTeam: 'Fleet CMD' },
-  { name: 'MonMon', EP: 20, matchesPlayed: 0, wins: 0, currentTeam: 'Fleet CMD' },
+  { name: 'dannyvye.com', EP: 22, matchesPlayed: 0, wins: 0, currentTeam: 'Fleet CMD' },
+  { name: 'MonMon', EP: 22, matchesPlayed: 0, wins: 0, currentTeam: 'Fleet CMD' },
   { name: 'holo_icy_holo', EP: 32, matchesPlayed: 4, wins: 2, currentTeam: '' },
 
   { name: 'Marvin', EP: 11, matchesPlayed: 0, wins: 0, currentTeam: 'TooSlow' },
@@ -62,8 +62,8 @@ const playerData = [
 
   
   { name: 'Air', EP: 42, matchesPlayed: 4, wins: 1, currentTeam: 'Frontier Negotiator' },
-  { name: 'aqua995', EP: 44, matchesPlayed: 4, wins: 1, currentTeam: 'Frontier Negotiator' },
-  { name: 'Fellingus', EP: 41, matchesPlayed: 3, wins: 1, currentTeam: 'Frontier Negotiator' },
+  { name: 'aqua995', EP: 49, matchesPlayed: 4, wins: 1, currentTeam: 'Frontier Negotiator' },
+  { name: 'Fellingus', EP: 46, matchesPlayed: 3, wins: 1, currentTeam: 'Frontier Negotiator' },
 ];
 
 export default playerData;

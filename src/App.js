2d279058ddd812e6f7568856005dd4ff2558a1b9
aquasaphir <aqua995.ste@gmail.com>
2026-06-23 23:26:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
       <main className="flex-grow container mx-auto p-4 tet">
         
         
-        <SwissBracket results={results} />
         <div className="Leaderboard"> <Leaderboard /> </div>
         <RulesSection />
         <SwissBracket results={results} />
